94bfd34b27977e7532a2efbd42a4cbe1b30bca05
Matt Propst <propstm@gmail.com>
2016-12-12 13:14:18 -0500

    initial commit - pre react

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head></head>
+	<body>
+		<div class="header">
+			<div class="backgroundImage"><img src="" alt="Matt Propst at Halibut Point State Park in Rockport, MA" />
+		</div>
+		<div class="content">
+			<div class="latest">
+				<!-- latest blog/paragraph/blurb will go here -->
+			</div>
+			<div class="skills">
+				<ul>
+					<li>JavaScript
+ 	   				<li>CSS3</li>
+    				<li>HTML5</li>
+	    			<li>Mobile & Responsive Design</li>
+    				<li>PHP</li>
+    				<li>MySQL</li>
+	    			<li>Objective-C / iOS</li>
+    			</ul>
+
+			</div>
+			<div class="currently">
+				<span class="heading">Actively Learning:</span> ReactJS 
+				<span class="heading">Reading:</span> Tools of Titans: The Tactics, Routines, and Habits of Billionaires, Icons, and World-Class Performers
+				<span class="heading">Listening To (Audiobook):</span>American Buffalo: In Search of a Lost Icon
+			</div>
+			<div class="footer">
+			</div>
+		</div>
+	</body>
+</html>
