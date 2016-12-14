80094f0ededef57f0ae632b3df372c65cd630790
Matt Propst <propstm@gmail.com>
2016-12-13 19:43:46 -0500

    added social images.

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,12 +8,14 @@ class Content extends React.Component{
 			<div className="contentContainer">
 				<Header />
 				<div className="about">
-					<p>I am a software developer living and working in Boston, MA. Over the past few years I've work in the LAMP Stack, also worked with MS SQL.  Along with web i've also been developing for iOS with Objective-C for the last several years.</p>
-					<p>Full stack is a loaded term (the stack seems to change so quickly as of late) but I enjoy being able to create a site an understand the data flow and architecture from front to back.</p>  
+					<p>I am a software developer living and working in Boston, MA. Over the past few years I've work in the LAMP Stack, also worked with MS SQL.  Along with web i've also been developing for iOS with Objective-C for the last several years. Full stack is a loaded term (the stack seems to change so quickly as of late) but I enjoy being able to create a site an understand the data flow and architecture from front to back.</p>  
+					<p>I decided to create this site because I didn't have an updated website or portfolio online, and this site could serve as a good sample project while learning React. While I may be missing a few features I know i'd like to continue to build out this site to include a blog, contact form, and portfolio.</p>
 				</div>
 				<div className="latest">
 				</div>
-				<Skills />
+				<div className="skills">
+					<p><span className="heading">Skills:</span> JavaScript, CSS3, HTML5, Mobile & Responsive Design, PHP, MySQL, Objective-C / iOS</p>
+				</div>
 				<div className="currently">
 					<p><span className="heading">Actively Learning:</span> ReactJS </p>
 					<p><span className="heading">Reading:</span> <a href="https://www.amazon.com/Tools-Titans-Billionaires-World-Class-Performers/dp/1328683788/ref=sr_1_1?ie=UTF8&qid=1481673272&sr=8-1&keywords=tools+of+titans" target="_blank">Tools of Titans: The Tactics, Routines, and Habits of Billionaires, Icons, and World-Class Performers</a></p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import linkedInImg from './../images/linkedin-logo.png'
+import stackOverflowImg from './../images/stack.png'
+import twitterImg from './../images/twitter-logo-silhouette.png'
+
 
 class Header extends React.Component{
 	render(){
 		return (
 			<div className="header">
-				<h2>MATT PROPST | SOFTWARE DEVELOPER</h2>
-			
-				<img src="./images/so-icon.png" alt="Stack Overflow Icon" />
+				<h1>MATT PROPST</h1>
+				<div className="socialLinks">
+					<img src={linkedInImg} alt="Stack Overflow Icon" />
+					<img src={stackOverflowImg} alt="Stack Overflow Icon" />
+					<img src={twitterImg} alt="Stack Overflow Icon" />
+				</div>
 			</div>
 			)
 	}
