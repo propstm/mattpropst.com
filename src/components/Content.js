7c0769c564e967eb0d69b787a760854d300ce332
Matt Propst <propstm@gmail.com>
2016-12-13 22:39:13 -0500

    rebuilt and trying to deploy again.

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,7 +17,7 @@ class Content extends React.Component{
 					<p><span className="heading">Skills:</span> JavaScript, CSS3, HTML5, Mobile & Responsive Design, PHP, MySQL, Objective-C / iOS</p>
 				</div>
 				<div className="currently">
-					<p><span className="heading">Actively Learning:</span> <a href="https://reactforbeginners.com/" target="_blank">ReactJS</a> </p>
+					<p><span className="heading">Actively Learning:</span> <a href="https://reactforbeginners.com/" target="_blank">ReactJS</a></p>
 					<p><span className="heading">Reading:</span> <a href="https://www.amazon.com/Tools-Titans-Billionaires-World-Class-Performers/dp/1328683788/ref=sr_1_1?ie=UTF8&qid=1481673272&sr=8-1&keywords=tools+of+titans" target="_blank">Tools of Titans: The Tactics, Routines, and Habits of Billionaires, Icons, and World-Class Performers</a></p>
 					<p><span className="heading">Listening To (Audiobook):</span> <a href="http://www.audible.com/pd/Science-Technology/American-Buffalo-Audiobook/B0036ZH8RE" target="_blank">American Buffalo: In Search of a Lost Icon</a></p>
 				</div>
